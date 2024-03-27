0de7ac56c9b3704b85a2fd09b9d9d74e4c871904
Ritik2513 <ritikgupta2513@gmail.com>
2024-03-27 11:57:23 +0530

    created board

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,44 @@
 class TicTacToe{
     char[][] board;
 
-    public TicTacToe(){
+    public TicTacToe()
+    {
         board = new char[3][3];
+        initBoard();
     }
+
+    void initBoard()
+    {
+        for (int i = 0; i < board.length; i++)
+        {
+            for(int j = 0; j < board[i].length; j++)
+            {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    void displayBoard()
+    {
+        System.out.println("-------------");
+        for (int i = 0; i < board.length; i++)
+        {
+            System.out.print("| ");
+            for(int j = 0; j < board[i].length; j++)
+            {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
 }
-
-
-
-
-
-
-
-
-
 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TicTacToe t = new TicTacToe();
+        t.displayBoard();
     }
 }
