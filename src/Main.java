aa447d44e8f641e3f13deb303cb2f3c0799c7c7a
Ritik2513 <ritikgupta2513@gmail.com>
2024-03-27 12:39:29 +0530

    first part done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-class TicTacToe{
+class TicTacToe
+{
     char[][] board;
 
     public TicTacToe()
@@ -33,6 +34,55 @@ class TicTacToe{
         }
     }
 
+    void placeMark(int row, int col, char mark)
+    {
+        if (row >= 0 && row <= 2 && col >= 0 && col <=2)
+        {
+            board[row][col] = mark;
+        }
+        else
+        {
+            System.out.println("Invalid Position");
+        }
+    }
+
+    boolean checkColWin()
+    {
+        for (int j = 0; j <= 2; j++)
+        {
+            if(board[0][j] != ' ' &&  board[0][j] == board[1][j] &&
+                    board[1][j] == board[2][j])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean checkRowWin()
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean checkDiagonalWin()
+    {
+        if(board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2] ||
+                board[0][2] != ' ' &&   board[0][2] == board[1][1] && board[1][1] == board[2][0])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
 
 
